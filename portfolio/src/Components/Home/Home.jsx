9739075b5d3db3fa3153760621f8f375d589b585
ThePS25 <pratyush2503@gmail.com>
2025-03-31 02:19:03 +0530

    ch

--- portfolio/src/Components/Home/Home.jsx
+++ portfolio/src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import TextChange from "../TextChange";
 
 const Home = () => {
   const resumeLink =
-    "https://drive.google.com/file/d/1LvdBCW9biQutsB97xBgrEumQOxG3jAI1/view";
+    "https://drive.google.com/file/d/1mqkq9UVspLKZWEWkgr_IWNZ87-MHbpV_/view";
   return (
     <div className="text-white flex w-full justify-around items-start p-6 md:p-10">
       <div className="md:w-2/4 md:pt-10 ">
